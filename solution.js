e86c68713e960e89db654874b29ae58f750500ba
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-16 13:12:20 +0200

    Add Q3 solution

--- solution.js
+++ solution.js
@@ -20,3 +20,52 @@
 		alert("The intergers are equal");
 		console.log("The integers are equal");
 	}
+
+//Question 2: For loop
+	for(var i = 0; i < 16; i++)
+	{
+		if((i%2)>0)
+		{
+			console.log(i+" is Odd.")
+		}
+		else
+		{
+			console.log(i+" is Even");
+		}
+	}
+ //Question 3: Sum multiples of 3 and 5 under 1000
+ 	//summing multiples of 3 under 1000
+ 	i = 1;
+ 	sumThree=0;
+ 	var prod3=0;
+ 	
+ 	while(prod3<1000)
+ 	{
+ 		prod3 = 3 * i;
+ 		if(prod3<1000)
+ 		{
+ 			sumThree += prod3;	
+ 		}
+ 		i+=1;
+ 	}
+ 	console.log("Sum of  multiples of 3 under 1000 is "+sumThree);
+ 	//summing the multiples of 5 under 1000
+ 	j = 1;
+ 	sumFive=0;
+ 	var prod5=0;
+ 	
+ 	while(prod5<1000)
+ 	{
+ 		prod5 = 3 * j;
+ 		if(prod5<1000)
+ 		{
+ 			sumFive += prod5;	
+ 		}
+ 		j+=1;
+ 	}
+ 	console.log("Sum of  multiples of 5 under 1000 is "+sumFive);
+ 	//Add both sums toger
+ 	var totalSum = sumThree + sumFive;
+ 	console.log("The sum of the multiples of 3 and 5 under 1000 is "+totalSum );
+ 	
+ 	
