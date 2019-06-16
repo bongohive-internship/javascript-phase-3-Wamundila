d208004b73773cc7c965171b7241e33fb436ea3b
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-16 12:10:14 +0200

    Add Q1 solution

--- solution.js
+++ solution.js
@@ -1,1 +1,22 @@
 // solutions to the exercises mentioned in the readme should be in this file.
+
+//Write a JavaScript program that accept two integers and display the larger.
+	//Enter integers
+	var int1 = prompt("Enter first integer.");
+	var int2 = prompt("Enter Second integer.");
+	//compare them
+	if(int1 > int2)
+	{
+		alert("The larger integer is "+int1);
+		console.log("The larger integer is "+int1);
+	}
+	else if(int1 < int2)
+	{
+		alert("The larger integer is "+int2);
+		console.log("The larger integer is "+int2);
+	}
+	else
+	{
+		alert("The intergers are equal");
+		console.log("The integers are equal");
+	}
